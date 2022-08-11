467a7ade5adbd71246db0cce309c4e8ce651486f
Ghost-Reborn <abhinanadammanamkandy@gmail.com>
2022-08-11 14:41:28 +0530

    Use ANIMECHRONO * To scrape episode details, movies, OVAs etc

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Anime Downloader Reborn")
+#
+# Anime Downloader Reborn
+#
+# Downloads full anime episodes, movies, OVA etc in
+# chronological order
+#
+
+# Get anime title
+anime_scraper_base_url = "https://www.animechrono.com/search?q="
+anime_name = input("Enter anime: ")
+anime_combined_url = anime_scraper_base_url + anime_name
